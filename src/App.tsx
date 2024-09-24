842e74fb4898a1ccf67ced28e1caec68ce64f1e3
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-24 09:38:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {chars} from "./Characters.ts"
+import './App.css'
+import RAndMGallery from "./components/RAndMGallery.tsx";
+
+function App() {
+  const [filter, setFilter] = useState<string>("")
+
+    const filteredList = chars
+        .filter((currywurst) => currywurst.name.toLowerCase().includes(filter.toLowerCase()))
+
+  return (
+    <>
+     <input type={"text"} onChange={(event) => setFilter(event.target.value)}
+     placeholder={"Search a Character!"}/>
+        {filteredList.length > 0 ?
+            <RAndMGallery chars={filteredList} /> : <p>No Characters Found!</p>}
+    </>
+  )
+}
+
+export default App
